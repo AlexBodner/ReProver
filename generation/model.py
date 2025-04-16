7e5052986a6723eef3dbf614e39759d9fc8e77d8
AlexBodner <61150961+AlexBodner@users.noreply.github.com>
2025-04-16 17:41:38 -0300

    Update model.py

--- generation/model.py
+++ generation/model.py
@@ -79,9 +79,10 @@ class RetrievalAugmentedGenerator(pl.LightningModule):
             self.retriever = None
         else:
             logger.info(f"Loading the retriever from {ret_ckpt_path}")
-            self.retriever = PremiseRetriever.load(
-                ret_ckpt_path, self.device, freeze=True
-            )
+            self.retriever = PremiseRetriever.load_hf(
+                ret_ckpt_path, max_seq_length = 1024, device = self.device
+            )#PremiseRetriever.load(
+                #ret_ckpt_path, self.device, freeze=True)
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.generator = T5ForConditionalGeneration.from_pretrained(model_name)
